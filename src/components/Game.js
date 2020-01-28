95c4780bc2183fe2ba640390a05c519e1add99ee
woeph7 <aeishin0212@gmail.com>
2020-01-28 18:24:54 +0100

    changeQuestion

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,38 +1,51 @@
 import React from 'react'
 import Question from '../components/Quesion'
 import { getQuiz } from '../utils/api'
-// const dummyQuestions = {
-//     question: "What's the best programming language ?!",
-//     answerChoices: ['Javascript', 'Java', 'C#', 'Swift'],
-//     answer: 0
-// }
 
 const Game = () => {
     const [currentQuestion, setCurrentQuestion] = React.useState(null)
+    // const [questions, setQuestion] = React.useState(null)
+
 
     React.useEffect(() => {
         let mounted = new AbortController();
+        let questions = [];
+
+
         const getData = async () => {
             try {
-                const questions = await getQuiz();
-                setCurrentQuestion(questions[0])
+                const data = await getQuiz()
+                questions = data;
+                changeQuestion()
+
             } catch (e) {
                 console.error(e)
             }
+            return questions
+        }
+        const changeQuestion = () => {
+            const questionIndex = Math.floor(
+                Math.random() * questions.length)
+            setCurrentQuestion(questions[questionIndex])
+
+            // remove question
+            const remainingQuestions = [...questions]
+            remainingQuestions.splice(questionIndex, 1)
+            questions = remainingQuestions
         }
 
         getData()
         return () => mounted.abort()
     }, [])
 
+
     return (
         <>
-            {
-                currentQuestion
-                    ?
-                    <Question question={currentQuestion} />
-                    :
-                    <p>loading...</p>
+            {currentQuestion
+                ?
+                <Question question={currentQuestion} />
+                :
+                <div id="loader"></div>
             }
         </>
     )
